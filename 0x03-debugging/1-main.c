1b75e91c67d12ef2d623d9b7ffb863754b9950aa
remedan <97916325+Remsaf@users.noreply.github.com>
2022-03-19 22:13:32 +0300

    My commit

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+* main - causes an infinite loop
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        printf("Infinite loop incoming :(\n");
+
+        i = 0;
+
+        while (i < 10)
+        {
+                putchar(i);
+        }
+
+        printf("Infinite loop avoided! \\o/\n");
+
+        return (0);
+}[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C
+[A[A[C[C[C[C[C[C[C[C[C[C
+
+
+[A[A[A[C[C[C[C[C[C[C
+
+[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C/*
+[C[C[C[C[C[C[C[C[ *while (i<10)
+         * [C[C[C*Infinite loop - No increase of variable
+            *putchar()[Di);[B[C[C[C[C[C[C[C[C[C[C[[C[C[C[C}
+            */[B[     
+             printf("Infinite loop avoided! \\o/\n");
+           
+                      return (0);
+
+}
