02cba9984085c4d52445bd480b91dba790a9f874
remedan <97916325+Remsaf@users.noreply.github.com>
2022-03-19 21:44:28 +0300

    My commit

---
+++ 0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+
+        return (0);
+}[A[A[A[A[C[C[C[C[C[C[C[C[C[C[C[C0[C[C0;[B[B[B
