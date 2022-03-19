77363d4466faade21b14bf5cc33154ff0fd544ea
remedan <97916325+Remsaf@users.noreply.github.com>
2022-03-19 23:04:28 +0300

    my commit message

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,18 +1,17 @@
-#include ""[Dmain.h"
+#include "main.h"
+
 /**
- *main - main
- [A[A[B[A[D	[B[C[C[C[C[C
- *Return: 0;
- */
+* main - main
+* Return: 0
+*/
 
 int main(void)
-	{
-[A[C[C[C[C[C[C{
-int i;
-i=0;
+{
+        int i;
+        i = 0;
 
-positive_or_negative(i);
+        positive_or_negative(i);
 
-return(0);
+	return (0);
 
 }
