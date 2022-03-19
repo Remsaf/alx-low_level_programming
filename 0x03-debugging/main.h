9f45ca7469d5033398cc4ee6277cb0a6f4b95b27
remedan <97916325+Remsaf@users.noreply.github.com>
2022-03-19 21:27:48 +0300

    My commit

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,8 +5,11 @@
 
 void positive_or_negative(int i);
 
-int largest_number(int a, int b, int c);
-void print_remaining_days(int month, int day, int year);
-int convert_day(int month, int day);
+#endif /* MAIN_H */[A[A[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+int largest number[D[D[D[D[D[D[D_[C[C[C[C[C[C(int a, int b, int c);
 
+[A[A[A[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
+
+[A[3~[3~[3~[3~[C[C[C[C[C[C[C[C[C[C[C[C[void print_remaining_days_(int month, int day, int year):
+int convert_day(int month, int day);
 #endif
