78f90adc1c13bb57bea8da5d183f90f6f4cfdc98
remedan <97916325+Remsaf@users.noreply.github.com>
2022-03-19 22:27:02 +0300

    my commit

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,28 +12,15 @@ int main(void)
         printf("Infinite loop incoming :(\n");
 
         i = 0;
-
-        while (i < 10)
-        {
-                putchar(i);
-        }
-
+        
+      /**
+       * while (i < 10)
+       * {
+       *        putchar(i);
+       * }
+       */
+	 
         printf("Infinite loop avoided! \\o/\n");
 
         return (0);
-}[A[A[A[A[A[A[A[A[C[C[C[C[C[C[C
-[A[A[C[C[C[C[C[C[C[C[C[C
-
-
-[A[A[A[C[C[C[C[C[C[C
-
-[C[C[C[C[C[C[C[C[C[C[C[C[C[C[A[C[C[C/*
-[C[C[C[C[C[C[C[C[ *while (i<10)
-         * [C[C[C*Infinite loop - No increase of variable
-            *putchar()[Di);[B[C[C[C[C[C[C[C[C[C[C[[C[C[C[C}
-            */[B[     
-             printf("Infinite loop avoided! \\o/\n");
-           
-                      return (0);
-
 }
