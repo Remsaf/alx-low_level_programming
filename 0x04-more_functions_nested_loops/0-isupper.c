a8bae8e310d619237027e9db7e4a50dfbea5747e
remedan <97916325+Remsaf@users.noreply.github.com>
2022-03-20 20:34:56 +0300

    my commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,19 +4,21 @@
  *@c; print int
  *Return: Always 0.
  */
- 
+
  int _isupper(int c)
  {
-     
-     if (c >= 'A' && c <= 'Z')
-        return(1);
- }
- else
- {
-     return(0);
- }
- 
- 
- 
- 
 
+     if (c >= 'A' && c <= 'Z')
+        {
+            return(1);
+        }
+         
+     else
+     {
+         return(0);
+     }
+
+ }
+
+
+	
