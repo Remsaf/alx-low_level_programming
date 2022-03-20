3b6969acded06d0fd77fe55d950c5087c939c9b8
remedan <97916325+Remsaf@users.noreply.github.com>
2022-03-20 19:20:53 +0300

    my commit

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h
+
+/**
+ *print_numbers - entry point
+ *
+ *Description: prints the numbers with _putchar
+ *
+ *return: void
+ */
+
+ void print_numbers(void)
+ {
+     int number;
+
+     for (number = '0'; number <= '9'; number++)
+     {
+         _putchar(number);
+     }
+     _putchar('\n');
+ }
+
