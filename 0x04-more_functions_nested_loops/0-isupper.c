83e3aebdf7c2777afe5befd9a6870b70c38bc257
remedan <97916325+Remsaf@users.noreply.github.com>
2022-03-20 20:49:50 +0300

    my commit

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -10,17 +10,14 @@
 
      if (c >= 'A' && c <= 'Z')
      {
-         return (1);
+         return(1);
      }
      else
      {
-         return (0);
+         return(0);
      }
-
  }
 
-
-}
 
 
 	
