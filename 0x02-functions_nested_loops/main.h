8e395406216084b3f5733231021618ee8265cca7
remedan <97916325+Remsaf@users.noreply.github.com>
2022-03-22 18:48:00 +0300

    my commit

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,5 +1,5 @@
-#ifndef main
-
+#ifndef _main_h_
+#define _main_h_
 int _putchar(char c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -13,5 +13,5 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);:x
 
-#define main
+
 #endif
